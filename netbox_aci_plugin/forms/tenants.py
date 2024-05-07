c2216cf9559dc5684f776c16487a2e9f0c57202a
Martin Hauser <git@pheus.dev>
2024-05-07 22:54:49 +0200

    feat(forms): Add grouping to ACI Tenant fields

--- netbox_aci_plugin/forms/tenants.py
+++ netbox_aci_plugin/forms/tenants.py
@@ -2,8 +2,10 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from django.utils.translation import gettext_lazy as _
 from netbox.forms import NetBoxModelForm
 from utilities.forms.fields import CommentField
+from utilities.forms.rendering import FieldSet
 
 from ..models.tenants import ACITenant
 
@@ -12,6 +14,10 @@ class ACITenantForm(NetBoxModelForm):
     """NetBox form for ACI Tenant model."""
 
     comments = CommentField()
+
+    fieldsets: tuple = (
+        FieldSet("name", "alias", "description", "tags", name=_("ACI Tenant")),
+    )
 
     class Meta:
         model = ACITenant
