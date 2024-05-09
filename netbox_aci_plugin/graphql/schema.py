bfc3adfa976bb50f30421dd954565adbe99e2f25
Martin Hauser <git@pheus.dev>
2024-05-09 22:56:37 +0200

    feat(graphql): Add support for ACI AppProfile

--- netbox_aci_plugin/graphql/schema.py
+++ netbox_aci_plugin/graphql/schema.py
@@ -6,8 +6,9 @@ from typing import List
 import strawberry
 import strawberry_django
 
+from ..models.tenant_app_profiles import ACIAppProfile
 from ..models.tenants import ACITenant
-from .types import ACITenantType
+from .types import ACIAppProfileType, ACITenantType
 
 
 @strawberry.type
@@ -21,6 +22,17 @@ class ACITenantsQuery:
     aci_tenant_list: List[ACITenantType] = strawberry_django.field()
 
 
-schema = [
-    ACITenantsQuery,
-]
+@strawberry.type
+class ACIAppProfilesQuery:
+    """GraphQL query definition for ACIAppProfile model."""
+
+    @strawberry.field
+    def aci_application_profile(self, id: int) -> ACIAppProfileType:
+        return ACIAppProfile.objects.get(pk=id)
+
+    aci_application_profile_list: List[ACIAppProfileType] = (
+        strawberry_django.field()
+    )
+
+
+schema = [ACITenantsQuery, ACIAppProfilesQuery]
