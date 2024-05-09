3fe0200803ac7bfd9b92dc0d605e971df885a1d7
Martin Hauser <git@pheus.dev>
2024-05-09 22:10:22 +0200

    feat(api): Add serializers for ACI AppProfile

--- netbox_aci_plugin/api/serializers.py
+++ netbox_aci_plugin/api/serializers.py
@@ -6,6 +6,7 @@ from netbox.api.serializers import NetBoxModelSerializer
 from rest_framework import serializers
 from tenancy.api.serializers import TenantSerializer
 
+from ..models.tenant_app_profiles import ACIAppProfile
 from ..models.tenants import ACITenant
 
 
@@ -40,5 +41,43 @@ class ACITenantSerializer(NetBoxModelSerializer):
             "name",
             "alias",
             "description",
+            "nb_tenant",
+        )
+
+
+class ACIAppProfileSerializer(NetBoxModelSerializer):
+    """Serializer for ACI Application Profile model."""
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_aci_plugin-api:aciappprofile-detail"
+    )
+    aci_tenant = ACITenantSerializer(nested=True, required=True)
+    nb_tenant = TenantSerializer(nested=True, required=False, allow_null=True)
+
+    class Meta:
+        model = ACIAppProfile
+        fields: tuple = (
+            "id",
+            "url",
+            "display",
+            "name",
+            "alias",
+            "description",
+            "aci_tenant",
+            "nb_tenant",
+            "comments",
+            "tags",
+            "custom_fields",
+            "created",
+            "last_updated",
+        )
+        brief_fields: tuple = (
+            "id",
+            "url",
+            "display",
+            "name",
+            "alias",
+            "description",
+            "aci_tenant",
             "nb_tenant",
         )
