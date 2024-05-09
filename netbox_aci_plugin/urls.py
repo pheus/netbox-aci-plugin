aa1be9a073d6c474efe97d7eafa9592b5b3c25e1
Martin Hauser <git@pheus.dev>
2024-05-09 21:40:35 +0200

    feat(urls): Add URL patterns for ACI AppProfile

--- netbox_aci_plugin/urls.py
+++ netbox_aci_plugin/urls.py
@@ -5,7 +5,14 @@
 from django.urls import path
 from netbox.views.generic import ObjectChangeLogView
 
+from .models.tenant_app_profiles import ACIAppProfile
 from .models.tenants import ACITenant
+from .views.tenant_app_profiles import (
+    ACIAppProfileDeleteView,
+    ACIAppProfileEditView,
+    ACIAppProfileListView,
+    ACIAppProfileView,
+)
 from .views.tenants import (
     ACITenantDeleteView,
     ACITenantEditView,
@@ -14,6 +21,7 @@ from .views.tenants import (
 )
 
 urlpatterns: tuple = (
+    # ACI Tenants
     path("tenant/", ACITenantListView.as_view(), name="acitenant_list"),
     path("tenant/add/", ACITenantEditView.as_view(), name="acitenant_add"),
     path("tenant/<int:pk>/", ACITenantView.as_view(), name="acitenant"),
@@ -32,5 +40,37 @@ urlpatterns: tuple = (
         ObjectChangeLogView.as_view(),
         name="acitenant_changelog",
         kwargs={"model": ACITenant},
+    ),
+    # ACI Application Profiles
+    path(
+        "app-profile/",
+        ACIAppProfileListView.as_view(),
+        name="aciappprofile_list",
+    ),
+    path(
+        "app-profile/add/",
+        ACIAppProfileEditView.as_view(),
+        name="aciappprofile_add",
+    ),
+    path(
+        "app-profile/<int:pk>/",
+        ACIAppProfileView.as_view(),
+        name="aciappprofile",
+    ),
+    path(
+        "app-profile/<int:pk>/edit/",
+        ACIAppProfileEditView.as_view(),
+        name="aciappprofile_edit",
+    ),
+    path(
+        "app-profile/<int:pk>/delete/",
+        ACIAppProfileDeleteView.as_view(),
+        name="aciappprofile_delete",
+    ),
+    path(
+        "app-profile/<int:pk>/changelog/",
+        ObjectChangeLogView.as_view(),
+        name="aciappprofile_changelog",
+        kwargs={"model": ACIAppProfile},
     ),
 )
