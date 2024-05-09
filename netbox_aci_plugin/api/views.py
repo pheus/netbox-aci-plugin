6ca5c5cdd7af8641d704ca811de5a8f25c7e912d
Martin Hauser <git@pheus.dev>
2024-05-09 22:11:20 +0200

    feat(api): Add API views for ACI AppProfile

--- netbox_aci_plugin/api/views.py
+++ netbox_aci_plugin/api/views.py
@@ -4,14 +4,26 @@
 
 from netbox.api.viewsets import NetBoxModelViewSet
 
+from ..filtersets.tenant_app_profiles import ACIAppProfileFilterSet
 from ..filtersets.tenants import ACITenantFilterSet
+from ..models.tenant_app_profiles import ACIAppProfile
 from ..models.tenants import ACITenant
-from .serializers import ACITenantSerializer
+from .serializers import ACIAppProfileSerializer, ACITenantSerializer
 
 
 class ACITenantListViewSet(NetBoxModelViewSet):
     """API view for listing ACI Tenant instances."""
 
-    queryset = ACITenant.objects.prefetch_related("tags")
+    queryset = ACITenant.objects.prefetch_related("nb_tenant", "tags")
     serializer_class = ACITenantSerializer
     filterset_class = ACITenantFilterSet
+
+
+class ACIAppProfileListViewSet(NetBoxModelViewSet):
+    """API view for listing ACI Application Profile instances."""
+
+    queryset = ACIAppProfile.objects.prefetch_related(
+        "aci_tenant", "nb_tenant", "tags"
+    )
+    serializer_class = ACIAppProfileSerializer
+    filterset_class = ACIAppProfileFilterSet
