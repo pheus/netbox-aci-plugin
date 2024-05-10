facbdfee1e67de1d8687f54243b387b1cf217024
Martin Hauser <git@pheus.dev>
2024-05-11 00:10:33 +0200

    fix(urls): Change model's path to plural

--- netbox_aci_plugin/urls.py
+++ netbox_aci_plugin/urls.py
@@ -22,53 +22,53 @@ from .views.tenants import (
 
 urlpatterns: tuple = (
     # ACI Tenants
-    path("tenant/", ACITenantListView.as_view(), name="acitenant_list"),
-    path("tenant/add/", ACITenantEditView.as_view(), name="acitenant_add"),
-    path("tenant/<int:pk>/", ACITenantView.as_view(), name="acitenant"),
+    path("tenants/", ACITenantListView.as_view(), name="acitenant_list"),
+    path("tenants/add/", ACITenantEditView.as_view(), name="acitenant_add"),
+    path("tenants/<int:pk>/", ACITenantView.as_view(), name="acitenant"),
     path(
-        "tenant/<int:pk>/edit/",
+        "tenants/<int:pk>/edit/",
         ACITenantEditView.as_view(),
         name="acitenant_edit",
     ),
     path(
-        "tenant/<int:pk>/delete/",
+        "tenants/<int:pk>/delete/",
         ACITenantDeleteView.as_view(),
         name="acitenant_delete",
     ),
     path(
-        "tenant/<int:pk>/changelog/",
+        "tenants/<int:pk>/changelog/",
         ObjectChangeLogView.as_view(),
         name="acitenant_changelog",
         kwargs={"model": ACITenant},
     ),
     # ACI Application Profiles
     path(
-        "app-profile/",
+        "app-profiles/",
         ACIAppProfileListView.as_view(),
         name="aciappprofile_list",
     ),
     path(
-        "app-profile/add/",
+        "app-profiles/add/",
         ACIAppProfileEditView.as_view(),
         name="aciappprofile_add",
     ),
     path(
-        "app-profile/<int:pk>/",
+        "app-profiles/<int:pk>/",
         ACIAppProfileView.as_view(),
         name="aciappprofile",
     ),
     path(
-        "app-profile/<int:pk>/edit/",
+        "app-profiles/<int:pk>/edit/",
         ACIAppProfileEditView.as_view(),
         name="aciappprofile_edit",
     ),
     path(
-        "app-profile/<int:pk>/delete/",
+        "app-profiles/<int:pk>/delete/",
         ACIAppProfileDeleteView.as_view(),
         name="aciappprofile_delete",
     ),
     path(
-        "app-profile/<int:pk>/changelog/",
+        "app-profiles/<int:pk>/changelog/",
         ObjectChangeLogView.as_view(),
         name="aciappprofile_changelog",
         kwargs={"model": ACIAppProfile},
