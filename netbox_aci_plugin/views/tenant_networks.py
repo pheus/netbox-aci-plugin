cfda5cf3e5596fbd174bd6261aa0dc080c341e71
Martin Hauser <git@pheus.dev>
2024-05-19 19:26:43 +0200

    feat(views): Add views for ACI VRF model

---
+++ netbox_aci_plugin/views/tenant_networks.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2024 Martin Hauser
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from netbox.views import generic
+from utilities.views import register_model_view
+
+from ..forms.tenant_networks import ACIVRFForm
+from ..models.tenant_networks import ACIVRF
+from ..tables.tenant_networks import ACIVRFTable
+
+
+@register_model_view(ACIVRF)
+class ACIVRFView(generic.ObjectView):
+    """Detail view for displaying a single object of ACI VRF."""
+
+    queryset = ACIVRF.objects.prefetch_related(
+        "aci_tenant",
+        "nb_tenant",
+        "tags",
+    )
+
+
+class ACIVRFListView(generic.ObjectListView):
+    """List view for listing all objects of ACI VRF."""
+
+    queryset = ACIVRF.objects.prefetch_related(
+        "aci_tenant",
+        "nb_tenant",
+        "tags",
+    )
+    table = ACIVRFTable
+
+
+@register_model_view(ACIVRF, "edit")
+class ACIVRFEditView(generic.ObjectEditView):
+    """Edit view for editing an object of ACI VRF."""
+
+    queryset = ACIVRF.objects.prefetch_related(
+        "aci_tenant",
+        "nb_tenant",
+        "tags",
+    )
+    form = ACIVRFForm
+
+
+@register_model_view(ACIVRF, "delete")
+class ACIVRFDeleteView(generic.ObjectDeleteView):
+    """Delete view for deleting an object of ACI VRF."""
+
+    queryset = ACIVRF.objects.prefetch_related(
+        "aci_tenant",
+        "nb_tenant",
+        "tags",
+    )
