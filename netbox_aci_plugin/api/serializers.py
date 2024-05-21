084895acf9c59d5913c56f3936e3d12d3224290b
Martin Hauser <git@pheus.dev>
2024-05-21 22:00:28 +0200

    feat(api): Add serializer for ACIBridgeDomain

--- netbox_aci_plugin/api/serializers.py
+++ netbox_aci_plugin/api/serializers.py
@@ -8,7 +8,7 @@ from rest_framework import serializers
 from tenancy.api.serializers import TenantSerializer
 
 from ..models.tenant_app_profiles import ACIAppProfile
-from ..models.tenant_networks import ACIVRF
+from ..models.tenant_networks import ACIVRF, ACIBridgeDomain
 from ..models.tenants import ACITenant
 
 
@@ -131,4 +131,62 @@ class ACIVRFSerializer(NetBoxModelSerializer):
             "aci_tenant",
             "nb_tenant",
             "nb_vrf",
+        )
+
+
+class ACIBridgeDomainSerializer(NetBoxModelSerializer):
+    """Serializer for ACI Bridge Domain model."""
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_aci_plugin-api:acibridgedomain-detail"
+    )
+    aci_vrf = ACIVRFSerializer(nested=True, required=True)
+    nb_tenant = TenantSerializer(nested=True, required=False, allow_null=True)
+
+    class Meta:
+        model = ACIBridgeDomain
+        fields: tuple = (
+            "id",
+            "url",
+            "display",
+            "name",
+            "alias",
+            "description",
+            "aci_vrf",
+            "nb_tenant",
+            "advertise_host_routes_enabled",
+            "arp_flooding_enabled",
+            "clear_remote_mac_enabled",
+            "dhcp_labels",
+            "ep_move_detection_enabled",
+            "igmp_interface_policy_name",
+            "igmp_snooping_policy_name",
+            "ip_data_plane_learning_enabled",
+            "limit_ip_learn_enabled",
+            "mac_address",
+            "multi_destination_flooding",
+            "pim_ipv4_enabled",
+            "pim_ipv4_destination_filter",
+            "pim_ipv4_source_filter",
+            "pim_ipv6_enabled",
+            "unicast_routing_enabled",
+            "unknown_ipv4_multicast",
+            "unknown_ipv6_multicast",
+            "unknown_unicast",
+            "virtual_mac_address",
+            "comments",
+            "tags",
+            "custom_fields",
+            "created",
+            "last_updated",
+        )
+        brief_fields: tuple = (
+            "id",
+            "url",
+            "display",
+            "name",
+            "alias",
+            "description",
+            "aci_vrf",
+            "nb_tenant",
         )
