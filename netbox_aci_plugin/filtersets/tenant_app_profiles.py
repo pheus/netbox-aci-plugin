87d1000860447f346492000d04db920dc03ff724
Martin Hauser <git@pheus.dev>
2024-06-06 21:15:47 +0200

    feat(filtersets): Add ACIEndpointGroup filterset

--- netbox_aci_plugin/filtersets/tenant_app_profiles.py
+++ netbox_aci_plugin/filtersets/tenant_app_profiles.py
@@ -8,7 +8,9 @@ from django.utils.translation import gettext_lazy as _
 from netbox.filtersets import NetBoxModelFilterSet
 from tenancy.models import Tenant
 
-from ..models.tenant_app_profiles import ACIAppProfile
+from ..choices import EPGQualityOfServiceClassChoices
+from ..models.tenant_app_profiles import ACIAppProfile, ACIEndpointGroup
+from ..models.tenant_networks import ACIVRF, ACIBridgeDomain
 from ..models.tenants import ACITenant
 
 
@@ -45,6 +47,99 @@ class ACIAppProfileFilterSet(NetBoxModelFilterSet):
             "description",
             "aci_tenant",
             "nb_tenant",
+        )
+
+    def search(self, queryset, name, value):
+        """Return a QuerySet filtered by the models description."""
+        if not value.strip():
+            return queryset
+        queryset_filter: Q = (
+            Q(name__icontains=value)
+            | Q(name_alias__icontains=value)
+            | Q(description__icontains=value)
+        )
+        return queryset.filter(queryset_filter)
+
+
+class ACIEndpointGroupFilterSet(NetBoxModelFilterSet):
+    """Filter set for ACI Endpoint Group model."""
+
+    aci_tenant = django_filters.ModelMultipleChoiceFilter(
+        field_name="aci_app_profile__aci_tenant",
+        queryset=ACITenant.objects.all(),
+        to_field_name="name",
+        label=_("ACI Tenant (name)"),
+    )
+    aci_tenant_id = django_filters.ModelMultipleChoiceFilter(
+        field_name="aci_app_profile__aci_tenant",
+        queryset=ACITenant.objects.all(),
+        to_field_name="id",
+        label=_("ACI Tenant (ID)"),
+    )
+    aci_app_profile = django_filters.ModelMultipleChoiceFilter(
+        queryset=ACIAppProfile.objects.all(),
+        to_field_name="name",
+        label=_("ACI Application Profile (name)"),
+    )
+    aci_app_profile_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=ACIAppProfile.objects.all(),
+        to_field_name="id",
+        label=_("ACI Application Profile (ID)"),
+    )
+    aci_vrf = django_filters.ModelMultipleChoiceFilter(
+        field_name="aci_bridge_domain__aci_vrf",
+        queryset=ACIVRF.objects.all(),
+        to_field_name="name",
+        label=_("ACI VRF (name)"),
+    )
+    aci_vrf_id = django_filters.ModelMultipleChoiceFilter(
+        field_name="aci_bridge_domain__aci_vrf",
+        queryset=ACIVRF.objects.all(),
+        to_field_name="id",
+        label=_("ACI VRF (ID)"),
+    )
+    aci_bridge_domain = django_filters.ModelMultipleChoiceFilter(
+        queryset=ACIBridgeDomain.objects.all(),
+        to_field_name="name",
+        label=_("ACI Bridge Domain (name)"),
+    )
+    aci_bridge_domain_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=ACIBridgeDomain.objects.all(),
+        to_field_name="id",
+        label=_("ACI Bridge Domain (ID)"),
+    )
+    nb_tenant = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tenant.objects.all(),
+        to_field_name="name",
+        label=_("NetBox tenant (name)"),
+    )
+    nb_tenant_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tenant.objects.all(),
+        to_field_name="id",
+        label=_("NetBox tenant (ID)"),
+    )
+    qos_class = django_filters.MultipleChoiceFilter(
+        choices=EPGQualityOfServiceClassChoices,
+        null_value=None,
+    )
+
+    class Meta:
+        model = ACIEndpointGroup
+        fields: tuple = (
+            "id",
+            "name",
+            "name_alias",
+            "description",
+            "aci_app_profile",
+            "aci_bridge_domain",
+            "nb_tenant",
+            "admin_shutdown",
+            "custom_qos_policy_name",
+            "flood_in_encap_enabled",
+            "intra_epg_isolation_enabled",
+            "qos_class",
+            "preferred_group_member_enabled",
+            "proxy_arp_enabled",
         )
 
     def search(self, queryset, name, value):
