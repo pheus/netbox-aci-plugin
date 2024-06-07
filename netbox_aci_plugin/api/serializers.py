a638f5ba939d842dd81b0fa9e8c146d11c66d7a5
Martin Hauser <git@pheus.dev>
2024-06-07 20:03:41 +0200

    feat(api): Add ACIEndpointGroup serializer

--- netbox_aci_plugin/api/serializers.py
+++ netbox_aci_plugin/api/serializers.py
@@ -7,7 +7,7 @@ from netbox.api.serializers import NetBoxModelSerializer
 from rest_framework import serializers
 from tenancy.api.serializers import TenantSerializer
 
-from ..models.tenant_app_profiles import ACIAppProfile
+from ..models.tenant_app_profiles import ACIAppProfile, ACIEndpointGroup
 from ..models.tenant_networks import (
     ACIVRF,
     ACIBridgeDomain,
@@ -242,6 +242,55 @@ class ACIBridgeDomainSubnetSerializer(NetBoxModelSerializer):
             "name_alias",
             "description",
             "gateway_ip_address",
+            "aci_bridge_domain",
+            "nb_tenant",
+        )
+
+
+class ACIEndpointGroupSerializer(NetBoxModelSerializer):
+    """Serializer for ACI Endpoint Group model."""
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_aci_plugin-api:aciendpointgroup-detail"
+    )
+    aci_app_profile = ACIAppProfileSerializer(nested=True, required=True)
+    aci_bridge_domain = ACIBridgeDomainSerializer(nested=True, required=True)
+    nb_tenant = TenantSerializer(nested=True, required=False, allow_null=True)
+
+    class Meta:
+        model = ACIEndpointGroup
+        fields: tuple = (
+            "id",
+            "url",
+            "display",
+            "name",
+            "name_alias",
+            "description",
+            "aci_app_profile",
+            "aci_bridge_domain",
+            "nb_tenant",
+            "description",
+            "admin_shutdown",
+            "custom_qos_policy_name",
+            "flood_in_encap_enabled",
+            "intra_epg_isolation_enabled",
+            "qos_class",
+            "preferred_group_member_enabled",
+            "proxy_arp_enabled",
+            "comments",
+            "tags",
+            "custom_fields",
+            "created",
+            "last_updated",
+        )
+        brief_fields: tuple = (
+            "id",
+            "url",
+            "display",
+            "name",
+            "name_alias",
+            "description",
+            "aci_app_profile",
             "aci_bridge_domain",
             "nb_tenant",
         )
