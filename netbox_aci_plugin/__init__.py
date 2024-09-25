fedac1a1467ba26a49d72db5180ea003a22fab20
Vincent Simonin <vincent@simonin.name>
2024-09-25 17:10:42 +0200

    feat: Add NetBox v4.1 compatibility
    Resolves: #2

--- netbox_aci_plugin/__init__.py
+++ netbox_aci_plugin/__init__.py
@@ -19,7 +19,7 @@ class ACIConfig(PluginConfig):
     author_email = __email__
     base_url = "aci"
     min_version = "4.0.0"
-    max_version = "4.0.99"
+    max_version = "4.1.99"
     default_settings = {
         "create_default_aci_tenants": True,
     }
