e1b6fe452f14f1890ccac50227fcadc12ae7c6a0
Martin Hauser <git@pheus.dev>
2024-11-22 22:01:48 +0100

    feat(tables): Add tables for ACI Contract Subjects
    Introduces `ACIContractSubjectTable` and
    `ACIContractSubjectReducedTable` to manage ACI Contract Subject data in
    the NetBox UI. These tables provide extensive and reduced views with
    customizable columns for flexibility.

--- netbox_aci_plugin/tables/tenant_contracts.py
+++ netbox_aci_plugin/tables/tenant_contracts.py
@@ -6,7 +6,7 @@ import django_tables2 as tables
 from django.utils.translation import gettext_lazy as _
 from netbox.tables import NetBoxTable, columns
 
-from ..models.tenant_contracts import ACIContract
+from ..models.tenant_contracts import ACIContract, ACIContractSubject
 
 
 class ACIContractTable(NetBoxTable):
@@ -62,4 +62,127 @@ class ACIContractTable(NetBoxTable):
             "description",
             "scope",
             "tags",
+        )
+
+
+class ACIContractSubjectTable(NetBoxTable):
+    """NetBox table for the ACI Contract Subject model."""
+
+    name = tables.Column(
+        verbose_name=_("Subject"),
+        linkify=True,
+    )
+    name_alias = tables.Column(
+        verbose_name=_("Alias"),
+        linkify=True,
+    )
+    aci_tenant = tables.Column(
+        verbose_name=_("ACI Tenant"),
+        linkify=True,
+    )
+    aci_contract = tables.Column(
+        verbose_name=_("Contract"),
+        linkify=True,
+    )
+    nb_tenant = tables.Column(
+        linkify=True,
+    )
+    apply_both_directions_enabled = columns.BooleanColumn(
+        verbose_name=_("Apply both directions"),
+    )
+    qos_class = columns.ChoiceFieldColumn(
+        verbose_name=_("QoS class"),
+    )
+    qos_class_cons_to_prov = columns.ChoiceFieldColumn(
+        verbose_name=_("QoS class (cons->prov)"),
+    )
+    qos_class_prov_to_cons = columns.ChoiceFieldColumn(
+        verbose_name=_("QoS class (prov->cons)"),
+    )
+    reverse_filter_ports_enabled = columns.BooleanColumn(
+        verbose_name=_("Reverse filter ports"),
+    )
+    service_graph_name = tables.Column(
+        verbose_name=_("Service Graph name"),
+    )
+    service_graph_name_cons_to_prov = tables.Column(
+        verbose_name=_("Service Graph name (cons->prov)"),
+    )
+    service_graph_name_prov_to_cons = tables.Column(
+        verbose_name=_("Service Graph name (prov->cons)"),
+    )
+    target_dscp = columns.ChoiceFieldColumn(
+        verbose_name=_("Target DSCP"),
+    )
+    target_dscp_cons_to_prov = columns.ChoiceFieldColumn(
+        verbose_name=_("Target DSCP (cons->prov)"),
+    )
+    target_dscp_prov_to_cons = columns.ChoiceFieldColumn(
+        verbose_name=_("Target DSCP (prov->cons)"),
+    )
+    tags = columns.TagColumn()
+    comments = columns.MarkdownColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIContractSubject
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_contract",
+            "nb_tenant",
+            "description",
+            "apply_both_directions_enabled",
+            "qos_class",
+            "qos_class_cons_to_prov",
+            "qos_class_prov_to_cons",
+            "reverse_filter_ports_enabled",
+            "service_graph_name",
+            "service_graph_name_cons_to_prov",
+            "service_graph_name_prov_to_cons",
+            "target_dscp",
+            "target_dscp_cons_to_prov",
+            "target_dscp_prov_to_cons",
+            "tags",
+            "comments",
+        )
+        default_columns: tuple = (
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_contract",
+            "apply_both_directions_enabled",
+            "reverse_filter_ports_enabled",
+            "service_graph_name",
+            "qos_class",
+            "target_dscp",
+            "description",
+            "tags",
+        )
+
+
+class ACIContractSubjectReducedTable(NetBoxTable):
+    """Reduced NetBox table for the ACI Contract Subject model."""
+
+    name = tables.Column(
+        verbose_name=_("Subject"),
+        linkify=True,
+    )
+    apply_both_directions_enabled = columns.BooleanColumn(
+        verbose_name=_("Apply both directions"),
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIContractSubject
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "apply_both_directions_enabled",
+        )
+        default_columns: tuple = (
+            "name",
+            "apply_both_directions_enabled",
         )
