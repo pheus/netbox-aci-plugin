7cc5026dcd7f32cdd6c73693884b34cd95f8ca1f
Martin Hauser <git@pheus.dev>
2025-03-14 20:11:18 +0100

    feat(tables): Add ACI uSeg Endpoint Group table
    Introduces a new table for managing ACI uSeg Endpoint Groups.

--- netbox_aci_plugin/tables/tenant/endpoint_groups.py
+++ netbox_aci_plugin/tables/tenant/endpoint_groups.py
@@ -6,7 +6,10 @@ import django_tables2 as tables
 from django.utils.translation import gettext_lazy as _
 from netbox.tables import NetBoxTable, columns
 
-from ...models.tenant.endpoint_groups import ACIEndpointGroup
+from ...models.tenant.endpoint_groups import (
+    ACIEndpointGroup,
+    ACIUSegEndpointGroup,
+)
 
 
 class ACIEndpointGroupTable(NetBoxTable):
@@ -58,6 +61,68 @@ class ACIEndpointGroupTable(NetBoxTable):
             "qos_class",
             "preferred_group_member_enabled",
             "proxy_arp_enabled",
+            "tags",
+            "comments",
+        )
+        default_columns: tuple = (
+            "name",
+            "name_alias",
+            "aci_app_profile",
+            "aci_bridge_domain",
+            "nb_tenant",
+            "description",
+            "tags",
+        )
+
+
+class ACIUSegEndpointGroupTable(NetBoxTable):
+    """NetBox table for the ACI uSeg Endpoint Group model."""
+
+    name = tables.Column(
+        verbose_name=_("uSeg EPG"),
+        linkify=True,
+    )
+    name_alias = tables.Column(
+        verbose_name=_("Alias"),
+        linkify=True,
+    )
+    aci_tenant = tables.Column(
+        verbose_name=_("ACI Tenant"),
+        linkify=True,
+    )
+    aci_app_profile = tables.Column(
+        linkify=True,
+    )
+    aci_bridge_domain = tables.Column(
+        linkify=True,
+    )
+    nb_tenant = tables.Column(
+        linkify=True,
+    )
+    qos_class = columns.ChoiceFieldColumn(
+        verbose_name=_("QoS class"),
+    )
+    tags = columns.TagColumn()
+    comments = columns.MarkdownColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIUSegEndpointGroup
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_app_profile",
+            "aci_bridge_domain",
+            "nb_tenant",
+            "description",
+            "admin_shutdown",
+            "custom_qos_policy_name",
+            "flood_in_encap_enabled",
+            "intra_epg_isolation_enabled",
+            "qos_class",
+            "preferred_group_member_enabled",
             "tags",
             "comments",
         )
