046fef615102314556d240896a0900e03e55c335
Martin Hauser <git@pheus.dev>
2025-03-14 20:19:48 +0100

    feat(tables): Add ACI uSeg Network Attribute table
    Introduces a new table for managing ACI uSeg Network Attributes.

--- netbox_aci_plugin/tables/tenant/endpoint_groups.py
+++ netbox_aci_plugin/tables/tenant/endpoint_groups.py
@@ -9,6 +9,7 @@ from netbox.tables import NetBoxTable, columns
 from ...models.tenant.endpoint_groups import (
     ACIEndpointGroup,
     ACIUSegEndpointGroup,
+    ACIUSegNetworkAttribute,
 )
 
 
@@ -133,5 +134,77 @@ class ACIUSegEndpointGroupTable(NetBoxTable):
             "aci_bridge_domain",
             "nb_tenant",
             "description",
+            "tags",
+        )
+
+
+class ACIUSegNetworkAttributeTable(NetBoxTable):
+    """NetBox table for the ACI uSeg Network Attribute model."""
+
+    name = tables.Column(
+        verbose_name=_("Network Attribute"),
+        linkify=True,
+    )
+    name_alias = tables.Column(
+        verbose_name=_("Alias"),
+        linkify=True,
+    )
+    aci_tenant = tables.Column(
+        verbose_name=_("ACI Tenant"),
+        linkify=True,
+    )
+    aci_app_profile = tables.Column(
+        verbose_name=_("ACI App Profile"),
+        linkify=True,
+    )
+    aci_useg_endpoint_group = tables.Column(
+        verbose_name=_("uSeg EPG"),
+        linkify=True,
+    )
+    attr_object_type = columns.ContentTypeColumn(
+        verbose_name=_("Attribute Type"),
+    )
+    attr_object = tables.Column(
+        verbose_name=_("Attribute"),
+        orderable=False,
+        linkify=True,
+    )
+    nb_tenant = tables.Column(
+        linkify=True,
+    )
+    type = columns.ChoiceFieldColumn(
+        verbose_name=_("Type"),
+    )
+    tags = columns.TagColumn()
+    comments = columns.MarkdownColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIUSegNetworkAttribute
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_app_profile",
+            "aci_useg_endpoint_group",
+            "attr_object_type",
+            "attr_object",
+            "nb_tenant",
+            "description",
+            "type",
+            "use_epg_subnet",
+            "tags",
+            "comments",
+        )
+        default_columns: tuple = (
+            "name",
+            "name_alias",
+            "aci_useg_endpoint_group",
+            "type",
+            "attr_object_type",
+            "attr_object",
+            "description",
+            "use_epg_subnet",
             "tags",
         )
