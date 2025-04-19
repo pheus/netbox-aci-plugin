339de6254c4ee3b6f24cbe921332891c8b7f8945
Martin Hauser <git@pheus.dev>
2025-04-19 15:23:49 +0200

    feat(tables): Add ESG Endpoint Group Selector table
    Introduces a new table for managing ACI ESG Endpoint Group Selectors.

--- netbox_aci_plugin/tables/tenant/endpoint_security_groups.py
+++ netbox_aci_plugin/tables/tenant/endpoint_security_groups.py
@@ -6,7 +6,10 @@ import django_tables2 as tables
 from django.utils.translation import gettext_lazy as _
 from netbox.tables import NetBoxTable, columns
 
-from ...models.tenant.endpoint_security_groups import ACIEndpointSecurityGroup
+from ...models.tenant.endpoint_security_groups import (
+    ACIEndpointSecurityGroup,
+    ACIEsgEndpointGroupSelector,
+)
 
 
 class ACIEndpointSecurityGroupTable(NetBoxTable):
@@ -60,6 +63,84 @@ class ACIEndpointSecurityGroupTable(NetBoxTable):
             "aci_app_profile",
             "aci_vrf",
             "nb_tenant",
+            "description",
+            "tags",
+        )
+
+
+class ACIEsgEndpointGroupSelectorTable(NetBoxTable):
+    """NetBox table for the ACI ESG Endpoint Group (EPG) Selector model."""
+
+    name = tables.Column(
+        verbose_name=_("EPG Selector"),
+        linkify=True,
+    )
+    name_alias = tables.Column(
+        verbose_name=_("Alias"),
+        linkify=True,
+    )
+    aci_tenant = tables.Column(
+        verbose_name=_("ACI Tenant (ESG)"),
+        linkify=True,
+    )
+    aci_app_profile = tables.Column(
+        verbose_name=_("ACI App Profile (ESG)"),
+        linkify=True,
+    )
+    aci_endpoint_security_group = tables.Column(
+        verbose_name=_("ACI ESG"),
+        linkify=True,
+    )
+    aci_epg_object_tenant = tables.Column(
+        verbose_name=_("ACI Tenant (EPG)"),
+        linkify=True,
+    )
+    aci_epg_object_app_profile = tables.Column(
+        verbose_name=_("ACI App Profile (EPG)"),
+        linkify=True,
+    )
+    aci_epg_object_type = columns.ContentTypeColumn(
+        verbose_name=_("ACI EPG Type"),
+    )
+    aci_epg_object = tables.Column(
+        verbose_name=_("ACI EPG"),
+        orderable=False,
+        linkify=True,
+    )
+    nb_tenant = tables.Column(
+        linkify=True,
+    )
+    tags = columns.TagColumn()
+    comments = columns.MarkdownColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIEsgEndpointGroupSelector
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_app_profile",
+            "aci_endpoint_security_group",
+            "aci_epg_object_tenant",
+            "aci_epg_object_app_profile",
+            "aci_epg_object_type",
+            "aci_epg_object",
+            "nb_tenant",
+            "description",
+            "tags",
+            "comments",
+        )
+        default_columns: tuple = (
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_app_profile",
+            "aci_endpoint_security_group",
+            "aci_epg_object_type",
+            "aci_epg_object_app_profile",
+            "aci_epg_object",
             "description",
             "tags",
         )
