e30e3792436dcd14418f4910221fa75a83068959
Martin Hauser <git@pheus.dev>
2025-04-19 15:41:06 +0200

    feat(tables): Add ESG Endpoint Selector table
    Introduces a new table for managing ACI ESG Endpoint Selectors.

--- netbox_aci_plugin/tables/tenant/endpoint_security_groups.py
+++ netbox_aci_plugin/tables/tenant/endpoint_security_groups.py
@@ -9,6 +9,7 @@ from netbox.tables import NetBoxTable, columns
 from ...models.tenant.endpoint_security_groups import (
     ACIEndpointSecurityGroup,
     ACIEsgEndpointGroupSelector,
+    ACIEsgEndpointSelector,
 )
 
 
@@ -141,6 +142,71 @@ class ACIEsgEndpointGroupSelectorTable(NetBoxTable):
             "aci_epg_object_type",
             "aci_epg_object_app_profile",
             "aci_epg_object",
+            "description",
+            "tags",
+        )
+
+
+class ACIEsgEndpointSelectorTable(NetBoxTable):
+    """NetBox table for the ACI ESG Endpoint Selector model."""
+
+    name = tables.Column(
+        verbose_name=_("Endpoint Selector"),
+        linkify=True,
+    )
+    name_alias = tables.Column(
+        verbose_name=_("Alias"),
+        linkify=True,
+    )
+    aci_tenant = tables.Column(
+        verbose_name=_("ACI Tenant"),
+        linkify=True,
+    )
+    aci_app_profile = tables.Column(
+        verbose_name=_("ACI App Profile"),
+        linkify=True,
+    )
+    aci_endpoint_security_group = tables.Column(
+        verbose_name=_("ACI ESG"),
+        linkify=True,
+    )
+    ep_object_type = columns.ContentTypeColumn(
+        verbose_name=_("Endpoint Type"),
+    )
+    ep_object = tables.Column(
+        verbose_name=_("Endpoint"),
+        orderable=False,
+        linkify=True,
+    )
+    nb_tenant = tables.Column(
+        linkify=True,
+    )
+    tags = columns.TagColumn()
+    comments = columns.MarkdownColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIEsgEndpointSelector
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "name_alias",
+            "aci_tenant",
+            "aci_app_profile",
+            "aci_endpoint_security_group",
+            "ep_object_type",
+            "ep_object",
+            "nb_tenant",
+            "description",
+            "tags",
+            "comments",
+        )
+        default_columns: tuple = (
+            "name",
+            "name_alias",
+            "aci_endpoint_security_group",
+            "ep_object_type",
+            "ep_object",
             "description",
             "tags",
         )
