ae1356cd49477f32199da169989e5d226f4ef72d
Martin Hauser <git@pheus.dev>
2025-08-31 01:32:11 +0200

    fix(plugin): Correct label and default setting keys
    Adds a `label` attribute to the plugin definition for better Django app
    compatibility. Fixes a typo in the default setting key for ACI contract
    filters to ensure consistency.

--- netbox_aci_plugin/__init__.py
+++ netbox_aci_plugin/__init__.py
@@ -12,6 +12,7 @@ class ACIConfig(PluginConfig):
     """NetBox ACI Plugin specific configuration."""
 
     name = "netbox_aci_plugin"
+    label = "netbox_aci_plugin"
     verbose_name = "NetBox ACI"
     description = "NetBox plugin for documenting Cisco ACI specific objects."
     version = __version__
@@ -22,7 +23,7 @@ class ACIConfig(PluginConfig):
     max_version = "4.3.99"
     default_settings = {
         "create_default_aci_tenants": True,
-        "create_default_aci_contracts_filters": True,
+        "create_default_aci_contract_filters": True,
     }
 
 
