78e54ad6e78c1de49ccd4273ec038d35e81436ca
Martin Hauser <git@pheus.dev>
2025-09-14 21:34:47 +0200

    feat(tables): Add ACIFabricTable for fabric overview
    Introduces `ACIFabricTable` to display ACI Fabric data within the NetBox
    plugin interface. Includes fields such as name, Fabric ID, Infra VLAN,
    GIPo Pool, scope, and related tenants. This enhances the user
    experience by providing a structured and linkable view of ACI Fabrics.

---
+++ netbox_aci_plugin/tables/fabric/fabrics.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: 2025 Martin Hauser
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import django_tables2 as tables
+from django.utils.translation import gettext_lazy as _
+from netbox.tables import NetBoxTable, columns
+
+from ...models.fabric.fabrics import ACIFabric
+
+
+class ACIFabricTable(NetBoxTable):
+    """NetBox table for the ACI Fabric model."""
+
+    name = tables.Column(
+        verbose_name=_("ACI Fabric"),
+        linkify=True,
+    )
+    fabric_id = tables.Column(
+        verbose_name=_("Fabric ID"),
+        linkify=True,
+    )
+    infra_vlan_vid = tables.Column(
+        verbose_name=_("Infra VID"),
+    )
+    infra_vlan = tables.Column(
+        verbose_name=_("Infra VLAN"),
+        linkify=True,
+    )
+    gipo_pool = tables.Column(
+        verbose_name=_("GIPo Pool"),
+        linkify=True,
+    )
+    scope_type = columns.ContentTypeColumn(
+        verbose_name=_("Scope Type"),
+    )
+    scope = tables.Column(
+        verbose_name=_("Scope"),
+        linkify=True,
+        orderable=False,
+    )
+    nb_tenant = tables.Column(
+        linkify=True,
+    )
+    tags = columns.TagColumn()
+    comments = columns.MarkdownColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = ACIFabric
+        fields: tuple = (
+            "pk",
+            "id",
+            "name",
+            "description",
+            "fabric_id",
+            "infra_vlan_vid",
+            "infra_vlan",
+            "gipo_pool",
+            "scope_type",
+            "scope",
+            "nb_tenant",
+            "tags",
+            "comments",
+        )
+        default_columns: tuple = (
+            "name",
+            "fabric_id",
+            "description",
+            "scope",
+            "nb_tenant",
+            "tags",
+        )
