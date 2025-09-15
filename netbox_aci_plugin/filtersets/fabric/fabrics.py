b6869719482970c121b14aab9d4c0d688da46383
Martin Hauser <git@pheus.dev>
2025-09-15 19:43:19 +0200

    feat(filtersets): Add ACIFabricFilterSet
    Introduces `ACIFabricFilterSet` to enable advanced filtering for
    ACI Fabric models. Includes filters for infrastructure VLANs,
    GIPo pools, and scoped tenant associations.

---
+++ netbox_aci_plugin/filtersets/fabric/fabrics.py
@@ -0,0 +1,61 @@
+# SPDX-FileCopyrightText: 2025 Martin Hauser
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import django_filters
+from dcim.base_filtersets import ScopedFilterSet
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from ipam.models import VLAN, Prefix
+from netbox.filtersets import NetBoxModelFilterSet
+
+from ...models.fabric.fabrics import ACIFabric
+from ..mixins import NBTenantFilterSetMixin
+
+
+class ACIFabricFilterSet(NBTenantFilterSetMixin, ScopedFilterSet, NetBoxModelFilterSet):
+    """Filter set for the ACI Fabric model."""
+
+    infra_vlan = django_filters.ModelMultipleChoiceFilter(
+        field_name="infra_vlan__vid",
+        queryset=VLAN.objects.all(),
+        to_field_name="vid",
+        label=_("Infrastructure VLAN (VID)"),
+    )
+    infra_vlan_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=VLAN.objects.all(),
+        to_field_name="id",
+        label=_("Infrastructure VLAN (ID)"),
+    )
+    gipo_pool = django_filters.ModelMultipleChoiceFilter(
+        field_name="gipo_pool__prefix",
+        queryset=Prefix.objects.all(),
+        to_field_name="prefix",
+        label=_("GIPo Pool (prefix)"),
+    )
+    gipo_pool_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=Prefix.objects.all(),
+        to_field_name="id",
+        label=_("GIPo Pool (ID)"),
+    )
+
+    class Meta:
+        model = ACIFabric
+        fields: tuple = (
+            "id",
+            "name",
+            "description",
+            "fabric_id",
+            "infra_vlan_vid",
+            "infra_vlan",
+            "gipo_pool",
+            "scope_type",
+            "nb_tenant",
+        )
+
+    def search(self, queryset, name, value):
+        """Return a QuerySet filtered by the model's description."""
+        if not value.strip():
+            return queryset
+        queryset_filter: Q = Q(name__icontains=value) | Q(description__icontains=value)
+        return queryset.filter(queryset_filter)
